f8ff767e00a6c096f3660a5962962946b3fcc1c9
tarde.cetafaju <tarde.cetafaju@PC03LAB2528>
2024-09-18 14:52:44 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Models/LivroContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public class LivroContext : DbContext 
+    {
+        public LivroContext(DbContextOptions<LivroContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Livro> Livro { get; set; } = null!;
+
+        public DbSet<WebApplication1.Models.Usuario> Usuario { get; set; } = default!;
+    }
+}
